ee94cc967199b92e915330f5b33b06f711db3891
faithleysath <faithleysath@gmail.com>
2025-01-12 21:44:50 +0800

    wip: 2025-01-12 21:44:50

--- test/event_system.py
+++ test/event_system.py
@@ -4,3 +4,5 @@ import asyncio
 
 async def handle_download(event: DownloadEvent):
     print(f'[handle_download] Downloading {event.url}...')
+    await asyncio.sleep(10)
+    print(f'[handle_download] Downloaded {event.url} to {event.path}')
