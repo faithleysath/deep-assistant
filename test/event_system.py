9f0c7b39681aee135044a81503afbd580ac0ffa6
faithleysath <faithleysath@gmail.com>
2025-01-12 21:46:40 +0800

    wip: 2025-01-12 21:46:40

--- test/event_system.py
+++ test/event_system.py
@@ -7,3 +7,16 @@ async def handle_download(event: DownloadEvent):
     print(f'[handle_download] Downloading {event.url}...')
     await asyncio.sleep(10)
     print(f'[handle_download] Downloaded {event.url} to {event.path}')
+
+async def main():
+    while True:
+        # read from user input
+        url = input('Enter URL: ')
+        path = input('Enter path: ')
+        event = DownloadEvent(url=url, path=path)
+        await EventManager.events.put(event)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(EventManager.run())
+    loop.run_until_complete(main())
