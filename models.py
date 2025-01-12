221412c046861b1b7b8715289b15f51f80e4f703
faithleysath <faithleysath@gmail.com>
2025-01-12 20:57:07 +0800

    wip: 2025-01-12 20:57:07

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
+import time
 
 class Event:
     def __init__(self, **raw_data):
         self.raw_data = raw_data
-        self.time = raw_data.get('time', )
+        self.time = raw_data.get('time', time.time())
