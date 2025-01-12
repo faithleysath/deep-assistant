3af415990376b6ddca66726b350e95ba33c468da
faithleysath <faithleysath@gmail.com>
2025-01-12 20:59:27 +0800

    wip: 2025-01-12 20:59:27

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 import time
+import uuid
 
 class Event:
     def __init__(self, **raw_data):
         self.raw_data = raw_data
         self.time = raw_data.get('time', time.time())
+        self.id = raw_data.get('id', uuid.uuid4())
