1826e3f3abe1a0af4ca3e0f77ae166a07c2eb263
faithleysath <faithleysath@gmail.com>
2025-01-12 21:29:59 +0800

    wip: 2025-01-12 21:29:59

--- core.py
+++ core.py
@@ -4,3 +4,8 @@ class EventManager:
     def __init__(self):
         self.events = asyncio.Queue()
         self.handlers = []
+
+    async def run(self):
+        while True:
+            event = await self.events.get()
+            print(f'Got event: {event}')
