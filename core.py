45c4442f4348353fa680b85ae61e8de0bdc7f98b
faithleysath <faithleysath@gmail.com>
2025-01-12 21:25:09 +0800

    wip: 2025-01-12 21:25:09

--- core.py
+++ core.py
@@ -2,11 +2,4 @@ import asyncio
 
 class EventManager:
     def __init__(self):
-        self.listeners = []
-    
-    def subscribe(self, listener):
-        self.listeners.append(listener)
-    
-    def notify(self, event):
-        for listener in self.listeners:
-            listener.notify(event)
+        self.listeners = asyncio.Queue()
