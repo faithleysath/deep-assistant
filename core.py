c4ce99bdda3d0655c8b6ce104f1b712f8b0a43fe
faithleysath <faithleysath@gmail.com>
2025-01-12 21:32:49 +0800

    wip: 2025-01-12 21:32:49

--- core.py
+++ core.py
@@ -5,8 +5,8 @@ class EventManager:
     events = asyncio.Queue()
     handlers = []
 
-    @staticmethod
-    async def run(self):
+    @classmethod
+    async def run(cls):
         while True:
-            event = await self.events.get()
+            event = await cls.events.get()
             print(f'Got event: {event}')
