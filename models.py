7f53228b8f74aee434c78b61714faeba0c619207
faithleysath <faithleysath@gmail.com>
2025-01-12 21:11:58 +0800

    wip: 2025-01-12 21:11:58

--- models.py
+++ models.py
@@ -15,6 +15,16 @@ class Event:
         self.status = raw_data.get('status', 'pending')
         self.name = raw_data.get('name', 'untitled')
         self.summary = raw_data.get('summary', 'no summary')
+        self.creator = raw_data.get('creator', 'unknown')
+        self.source = raw_data.get('source', 'unknown')
 
     def __str__(self):
-        return f'{}
+        return f'{datetime.datetime.fromtimestamp(self.time)}: {self.name} ({self.status})'
+
+    def __repr__(self):
+        return str(self)
+
+class DownloadEvent(Event):
+    def __init__(self, **raw_data):
+        super().__init__(**raw_data)
+        
