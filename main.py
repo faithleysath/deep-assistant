94490a4764aa693b97e195e65a60724de9565e26
faithleysath <faithleysath@gmail.com>
2025-01-13 17:04:16 +0800

    wip: 2025-01-13 17:04:16

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import asyncio
 from core import EventManager
-from models import PrivateMessageEvent, MessageEvent
+from models import MessageEvent
 from qqws import listen_message
 
 @EventManager.register()
-async def handle_message(event: PrivateMessageEvent):
+async def handle_message(event: MessageEvent):
     print(event.message)
 
 loop = asyncio.get_event_loop()
