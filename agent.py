c756b37e9366839b20788d716b6640a1bf87f7a9
faithleysath <faithleysath@gmail.com>
2025-01-13 18:29:43 +0800

    wip: 2025-01-13 18:29:43

--- agent.py
+++ agent.py
@@ -4,3 +4,4 @@ base_path = os.path.dirname(os.path.realpath(__file__))
 class Agent:
     def __init__(self, name):
         self.name = name
+        self.prompt_file = os.path.join(base_path, 'agents', name, 'prompt.txt')
