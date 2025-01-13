4091e79f01da4b6fd56782421da981a3e179352d
faithleysath <faithleysath@gmail.com>
2025-01-13 22:03:54 +0800

    wip: 2025-01-13 22:03:54

--- test.py
+++ test.py
@@ -210,6 +210,7 @@ user: I'm a software engineer, recommad me a book.
 If you don't know user's job before, you should store it first. After that, you can answer user's question.
 You must force myself to remember user's information as much as possible. About user's everything, like job, hobby, family, even user's watched movie or read book.
 Each time you should call add_or_update_memory function to store user's information, unless there's no more information to store.
+When a key can have multiple values, you should store them as a list. For example, user's hobby can be multiple, you should store them as a list.
 """
 
 # 多轮对话
