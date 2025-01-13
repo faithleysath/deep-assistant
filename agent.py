68a0ac0affe15dfae0712a4674ca28799696c4f1
faithleysath <faithleysath@gmail.com>
2025-01-13 18:32:03 +0800

    wip: 2025-01-13 18:32:03

--- agent.py
+++ agent.py
@@ -6,3 +6,7 @@ class Agent:
     def __init__(self, name):
         self.name = name
         self.prompt_file = os.path.join(base_path, 'agents', name, 'prompt.txt')
+
+    def get_prompt(self):
+        with open(self.prompt_file, 'r') as f:
+            return f.read()
