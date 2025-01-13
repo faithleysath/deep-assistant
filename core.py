2a25fc64fc38d283621ca087568b18828a91de1e
faithleysath <faithleysath@gmail.com>
2025-01-13 15:44:32 +0800

    wip: 2025-01-13 15:44:32

--- core.py
+++ core.py
@@ -40,7 +40,7 @@ class EventManager:
     @classmethod
     async def add_event(cls, event: Event):
         if event.status == EventStatus.PENDING:
-            if event.trigger_num == 0:
+            if event.trigger_num < 1:
                 await cls.immediate_events.put(event)
             else:
                 await cls.delayed_events.put(event)
