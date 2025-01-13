70980c25dcb755ccefcc9efeaf0a2033a940cd1b
faithleysath <faithleysath@gmail.com>
2025-01-13 15:39:02 +0800

    wip: 2025-01-13 15:39:02

--- core.py
+++ core.py
@@ -32,6 +32,7 @@ class EventManager:
     async def handle_event(cls, event: Event):
         for event_type, handler, priority in sorted(cls.handlers, key=lambda x: x[2]):
             if isinstance(event, event_type):
+                result = await handler(event)
                 
 
     @classmethod
