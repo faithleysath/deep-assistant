1c52610a4a1fcaa891df76691dd2b6d9edeef660
faithleysath <faithleysath@gmail.com>
2025-01-13 20:02:28 +0800

    wip: 2025-01-13 20:02:28

--- test.py
+++ test.py
@@ -369,7 +369,7 @@ def chat():
 
         # 更新 messages[0] 为当前记忆概览
         memory_summary = memory_manager.get_summary()
-        messages[0] = prompt.format(memory_summary=json.dumps(memory_summary, ensure_ascii=False))
+        messages[0] = {"role": "system", "content": prompt.format(memory_summary=json.dumps(memory_summary, indent=4, ensure_ascii=False))}
 
         # 添加用户输入到消息历史
         messages.append({"role": "user", "content": user_input})
