70181750f2ea3fcdeedff21ea03138110f1f50cc
faithleysath <faithleysath@gmail.com>
2025-01-13 19:12:05 +0800

    wip: 2025-01-13 19:12:05

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from openai import OpenAI
 
+client = OpenAI(
+    api_key="sk-c5ea9ba45f3f4fdbb55c2d6a32399a57",
+    base_url="https://api.deepseek.com",
+)
+
 def send_messages(messages):
     response = client.chat.completions.create(
         model="deepseek-chat",
@@ -9,17 +14,3 @@ def send_messages(messages):
     print(response)
     return response.choices[0].message
 
-client = OpenAI(
-    api_key="sk-c5ea9ba45f3f4fdbb55c2d6a32399a57",
-    base_url="https://api.deepseek.com",
-)
-response = client.chat.completions.create(
-    model="deepseek-chat",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Hello"},
-    ],
-    stream=False
-)
-
-print(response.choices[0].message.content)
