018425c25813bd9d2d92e50b8e56495dc9843127
faithleysath <faithleysath@gmail.com>
2025-01-15 22:58:44 +0800

    wip: 2025-01-15 22:58:44

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 import asyncio
 from app.core import EventManager
-from app.models import MessageEvent
+from app.models import PrivateMessageEvent
 from app.qqws import listen_message
 
 @EventManager.register()
-async def handle_message(event: MessageEvent):
-    print(event.message)
+async def handle_message(event: Priv
 
 loop = asyncio.get_event_loop()
 loop.create_task(listen_message())
