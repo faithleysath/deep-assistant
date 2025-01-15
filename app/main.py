5250b213b31822eb1ea4e52c4be0f4926cea3b88
faithleysath <faithleysath@gmail.com>
2025-01-15 22:57:34 +0800

    wip: 2025-01-15 22:57:34

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import asyncio
-from src.core import EventManager
-from src.models import MessageEvent
-from src.qqws import listen_message
+from app.core import EventManager
+from app.models import MessageEvent
+from sapp.qqws import listen_message
 
 @EventManager.register()
 async def handle_message(event: MessageEvent):
