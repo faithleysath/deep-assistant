efd3c67952ae294750313e3edc4448e076753068
faithleysath <faithleysath@gmail.com>
2025-01-15 23:08:04 +0800

    wip: 2025-01-15 23:08:04

--- app/main.py
+++ app/main.py
@@ -5,10 +5,18 @@ from app.qqws import listen_message
 from app.config import config
 
 @EventManager.register()
-async def handle_user_message(event: PrivateMessageEvent):
+async def handle_qq_message(event: PrivateMessageEvent):
     message = event.message
     if message.user_id == config.get("user_id"):
         return
+    
+@EventManager.register()
+async def handle_group_message(event: PrivateMessageEvent):
+    message = event.message
+    if message.user_id == config.get("user_id"):
+        return
+    if message.raw_message == "你好":
+        await event.reply("你好呀！")
 
 loop = asyncio.get_event_loop()
 loop.create_task(listen_message())
