f522ca2f8cc9e0514ed8b350b3e54c6390b927cb
faithleysath <faithleysath@gmail.com>
2025-01-15 23:00:04 +0800

    wip: 2025-01-15 23:00:04

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ from app.qqws import listen_message
 from app.config import config
 
 @EventManager.register()
-async def handle_message(event: Priv
+async def handle_message(event: PrivateMessageEvent):
+    message = event.message
+    if message.user_id == config.get("user_id"):
+        print("来自用户的消息", message.raw_message)
 
 loop = asyncio.get_event_loop()
 loop.create_task(listen_message())
