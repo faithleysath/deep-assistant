be06c03c3343f8439f0fee828303ba55f088d865
faithleysath <faithleysath@gmail.com>
2025-01-15 23:09:04 +0800

    wip: 2025-01-15 23:09:04

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ async def handle_qq_message(event: PrivateMessageEvent):
     
 @EventManager.register()
 async def handle_user_message(event: UserMessageEvent):
+    agent = Agent("master")
 
 loop = asyncio.get_event_loop()
 loop.create_task(listen_message())
